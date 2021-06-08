9095fa47465b96eb9ce190191c3a7a5e4b3f857c
kenan <kenan@LAPTOP-5KTTCAE1>
2021-06-08 10:24:33 +0200

    Add project files.

---
+++ MVC_Party/Controllers/VisitorController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Party.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MVC_Party.Controllers
+{
+    public class VisitorsController : Controller
+    {
+        private static List<int> list = new List<int>();
+        public IActionResult Index()
+        {
+            TempData["IsFamily"] = 0;
+            TempData["MinAge"] = 0;
+            TempData["MaxAge"] = 0;
+            TempData["TotalVisitors"] = 0;
+
+            return View();
+        }
+        public IActionResult Summary()
+        {
+            var max = list.Max();
+            TempData["MaxAge"] = max;
+
+            var min = list.Min();
+            TempData["MinAge"] = min;
+
+            var total = list.Count();
+            TempData["TotalVisitors"] = total;
+
+            return View("Summary");
+        }
+        [HttpPost]
+        public IActionResult Index(Visitor visitor)
+        {
+            if (ModelState.IsValid)
+            {
+                int isFamily = int.Parse(TempData.Peek("IsFamily").ToString());
+
+                if (visitor.IsFamily)
+                {
+                    isFamily++;
+                    TempData["IsFamily"] = isFamily;
+                }
+
+                list.Add(visitor.Age);
+            }
+            return View();
+        }
+    }
+}
---
+++ MVC_Party/Models/Visitor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Party.Models
+{
+    public class Visitor
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public int PostalCode { get; set; }
+        public string City { get; set; }
+        public bool IsFamily { get; set; }
+    }
+}
